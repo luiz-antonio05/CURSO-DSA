a78e1b968df8510319a6a196810cf5e2e1660c4d
luiz-antonio05 <137508484+luiz-antonio05@users.noreply.github.com>
2023-06-23 09:23:10 -0300

    Delete jooj.py

--- jooj.py
+++
@@ -1,6 +0,0 @@
-nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
-cidade = input("Digite sua cidade: ")
-
-frase = "Olá, meu nome é {} e tenho {} anos. Eu moro em {}.".format(nome, idade, cidade)
-print(frase)
