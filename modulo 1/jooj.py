cd9d3b86bd61f222378840a541578ccfbed2b665
luiz-antonio05 <137508484+luiz-antonio05@users.noreply.github.com>
2023-06-23 09:22:52 -0300

    Add files via upload

---
+++ modulo 1/jooj.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite sua cidade: ")
+
+frase = "Olá, meu nome é {} e tenho {} anos. Eu moro em {}.".format(nome, idade, cidade)
+print(frase)
